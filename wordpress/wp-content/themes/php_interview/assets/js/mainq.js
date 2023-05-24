52911b965393f75cb5205353e3749dd0ff31d20a
Pavlo Karpunin <32207942+KarpuninP@users.noreply.github.com>
2023-05-24 19:24:15 +0300

    Update mainq.js

--- wordpress/wp-content/themes/php_interview/assets/js/mainq.js
+++ wordpress/wp-content/themes/php_interview/assets/js/mainq.js
@@ -2,31 +2,37 @@ $(document).ready(function() {
     // Modal window initialization
     $('#signIn').modal();
 
-    // Get reference to carousel container
-    var carouselContainer = $('.carousel-container');
-    var carouselTrack = carouselContainer.find('.carousel-track');
-    var carouselItems = carouselContainer.find('.carousel-block');
-    var currentIndex = 0;
-    var direction = 1; // Direction of movement (1 - forward, -1 - backward)
+// Get reference to carousel container
+var carouselContainer = document.querySelector('.carousel-container');
+var carouselTrack = carouselContainer.querySelector('.carousel-track');
+var carouselItems = carouselContainer.querySelectorAll('.carousel-block');
+var carouselWidth = carouselContainer.offsetWidth;
+var currentIndex = 0;
+var direction = 1;  // Direction of movement (1 - forward, -1 - backward)
 
+if (carouselItems.length > 0) { // Check if carouselItems is not empty
     // Clone each item and append it to the end of the track
-    carouselItems.clone().appendTo(carouselTrack);
+    carouselItems.forEach(function(item) {
+        carouselTrack.appendChild(item.cloneNode(true));
+    });
 
     // Set the width of the track equal to the total width of the carousel
     function recalculateCarouselWidth() {
-        var carouselWidth = carouselContainer.width();
+        var carouselWidth = carouselContainer.offsetWidth;
         var totalWidth = 0;
 
-        carouselItems.each(function() {
-            totalWidth += $(this).outerWidth();
+        carouselItems.forEach(function(item) {
+            totalWidth += item.offsetWidth;
         });
 
         var neededCopies = Math.ceil(carouselWidth / totalWidth) + 1;
         for (var i = 0; i < neededCopies; i++) {
-            carouselItems.clone().appendTo(carouselTrack);
+            carouselItems.forEach(function(item) {
+                carouselTrack.appendChild(item.cloneNode(true));
+            });
         }
 
-        carouselTrack.width(totalWidth * neededCopies);
+        carouselTrack.style.width = totalWidth * neededCopies + 'px';
     }
 
     recalculateCarouselWidth();
@@ -34,26 +40,25 @@ $(document).ready(function() {
     // Animate the carousel
     function animateCarousel() {
         setInterval(function() {
-            var itemWidth = carouselItems.eq(currentIndex).outerWidth();
+            var itemWidth = carouselItems[currentIndex].offsetWidth;
             var trackOffset = -itemWidth * currentIndex;
 
-            carouselTrack.css('transform', 'translateX(' + trackOffset + 'px)');
+            carouselTrack.style.transform = 'translateX(' + trackOffset + 'px)';
 
             currentIndex += direction;
 
             if (currentIndex >= carouselItems.length) {
                 currentIndex = 0;
-                carouselTrack.css('transition', 'none');
-                carouselTrack.css('transform', 'translateX(0)');
+                carouselTrack.style.transition = 'none';
+                carouselTrack.style.transform = 'translateX(0)';
                 setTimeout(function() {
-                    carouselTrack.css('transition', 'transform 0.5s ease-in-out');
-                    carouselTrack.css('transform', 'translateX(' + (-carouselItems.eq(currentIndex).position().left) + 'px)');
+                    carouselTrack.style.transition = 'transform 1.5s ease-in-out';
+                    carouselTrack.style.transform = 'translateX(' + (-carouselItems[currentIndex].offsetLeft) + 'px)';
                 }, 10);
             }
-        }, 5000);
+        }, 10000);
     }
-
-    // Call the function to animate the carousel
     animateCarousel();
-});
+}
 
+});
